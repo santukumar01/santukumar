d3ef48b8d118fa6d7015e7a1f048cfe5834a1c3d
santukumar01 <santu13102000@gmail.com>
2024-10-23 11:48:59 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Navbar from "./sections/Navbar";
+import Hero from "./sections/Hero.jsx";
+import About from "./sections/About.jsx";
+import Projects from "./sections/Project.jsx";
+import Clients from "./sections/Clients.jsx";
+import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
+import WorkExperience from "./sections/Experiance.jsx";
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar />
+      <Hero />
+      <About />
+      <Projects />
+      <Clients />
+      <WorkExperience />
+      <Contact />
+      <Footer />
+    </main>
+  );
+};
+
+export default App;
