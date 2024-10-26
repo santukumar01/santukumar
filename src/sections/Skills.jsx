46fe4c286b917ab640d1176ee54e1dc0655e84c7
santukumar01 <santu13102000@gmail.com>
2024-10-26 19:13:42 +0530

    final

--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -25,16 +25,15 @@ const Skills = () => {
 
   return (
     <section className="c-space my-20" id="skills">
-      {/* <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>; */}
       <div className="col-span-1 xl:row-span-3">
         <div className="grid-container">
           <p className="grid-headtext">My Skills</p>
 
-          <div className="flex justify-start space-x-4">
+          <div className="flex justify-start flex-wrap space-x-4">
             {icons.map((icon, index) => (
               <div
                 key={index}
-                className={`p-6  text-white cursor-pointer rounded-lg`} // Rounded corners, square shape
+                className="p-6 text-white cursor-pointer rounded-lg"
                 onMouseEnter={() => handleMouseEnter(index)}
                 onMouseLeave={handleMouseLeave}
                 style={{
@@ -43,18 +42,18 @@ const Skills = () => {
                     hoveredIndex === index
                       ? "rotateY(180deg)"
                       : "rotateY(0deg)",
-                  perspective: "1000px", // 3D perspective for rotation
+                  perspective: "1000px",
                   display: "flex",
                   justifyContent: "center",
                   alignItems: "center",
-                  width: "100px", // Larger square size (width and height)
+                  width: "100px",
                   height: "100px",
                 }}
               >
                 <div
                   style={{
-                    fontSize: "40px", // Larger icon size
-                    backfaceVisibility: "hidden", // Hide back of icon during rotation
+                    fontSize: "40px",
+                    backfaceVisibility: "hidden",
                   }}
                 >
                   <box-icon
@@ -63,12 +62,10 @@ const Skills = () => {
                     color={icon.color}
                     size="50px"
                   ></box-icon>
-                  {/* <i class='bx bxs-heart bx-border-circle'></i> */}
                 </div>
               </div>
             ))}
           </div>
-          <div></div>
         </div>
       </div>
     </section>
